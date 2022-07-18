838683abf4cf3382f11804b8325bcce9ebbc6571
DESKTOP-2NV54CR\ayj71 <ayj7137@naver.com>
2022-07-18 22:05:27 +0900

    jdbcPractice220718

--- jdbcPractice220718/src/main/Main.java
+++ jdbcPractice220718/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.util.Scanner;
 
 import member.MemberController;
+import member.MemberDto;
 
 public class Main {
 
@@ -18,15 +19,11 @@ public class Main {
 		System.out.println("3.마이페이지");
 		
 		String input = sc.nextLine();
-		
+		MemberDto dto = null;
 		switch(input) {
 		case "1" : 
-			System.out.println("===== 아래에 로그인 정보를 입력해주세요. =====");
-			System.out.print("아이디 : ");
-			String id = sc.nextLine();
-			System.out.print("비밀번호 : ");
-			String pwd = sc.nextLine();
-			mc.login(id,pwd);
+			dto = mc.login();
+			mc.loginCheck(dto);//메서드로 분리
 			break;
 		case "2" : 
 			int result = mc.join();
@@ -34,7 +31,6 @@ public class Main {
 			else System.out.println("회원가입 실패...");
 			break;
 		case "3" : 
-			
 			mc.edit();
 			break;
 		default : System.out.println("그런 숫자 없어요");
