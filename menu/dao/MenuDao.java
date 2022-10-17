170d6f67364c511e91ac8feac5201b3dba64c8e3
DESKTOP-2NV54CR\ayj71 <ayj7137@naver.com>
2022-10-17 23:53:12 +0900

    WebPractice221017

---
+++ menu/dao/MenuDao.java
@@ -0,0 +1,16 @@
+package com.kh.cook.menu.dao;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.kh.cook.menu.vo.MenuVo;
+
+public class MenuDao {
+
+	//디저트 리스트 조회
+	public List<MenuVo> selectDessertList(Connection conn) {
+		string sql = "";
+		
+	}
+
+}
